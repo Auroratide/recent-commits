223252c44e7ad694a8e013caf63e8d3133004a90
Timothy Foster <tf.auroratide@gmail.com>
2023-03-18 15:13:40 -0500

    Fix 'unknown revision or path not in the working tree' error

--- recent-commits.sh
+++ recent-commits.sh
@@ -6,7 +6,7 @@ BASE_URL=$1
 FROM_COMMIT=$2
 TO_COMMIT=$3
 
-REFS=$(git rev-list --format=oneline $FROM_COMMIT..$TO_COMMIT)
+REFS=$(git rev-list --format=oneline $FROM_COMMIT..$TO_COMMIT --)
 while read -r commit; do
 	hash=$(echo "$commit" | cut -d' ' -f1)
 	message=$(echo "$commit" | cut -d' ' -f2-)
